c0d364b18751857fd03ae4056ff739969e681c4e
stealthcamaro <lujanuriel@icloud.com>
2025-12-23 23:11:28 -0600

    Added more variable examples

--- variables.cpp
+++ variables.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main(){
+using namespace std;
 
+int main(){
+    int alpha = 67;
+    double beta = 24.125;
+    char charlie = 'C';
+    string delta = "F-22 Raptor";
+    bool echo = false;
+    cout << "This is the integer variable. This variable is used to store whole numbers such as: " << alpha << endl;
+    cout << "This is the double variable. This variable is used to store floating point numbers with decimals such as: " << beta << endl;
+    cout << "This is the char variable meant to store single characters like: " << charlie << endl;
+    cout << "This is the string variable meant to store text like: " << delta << endl;
+    cout << "This is the bool variable meant to store either a TRUE(1) or FALSE(0) value like this: " << echo << endl;
     return 0;
 }
