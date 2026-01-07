7e75f49911e67ddd1469f2bb9d1f8e05bbbeac38
stealthcamaro <lujanuriel@icloud.com>
2026-01-07 01:38:23 -0600

    Added menu feature to make the CLI app user friendly. Refactored functions to reflect this change and detected an input validation bug that needs to be addressed when a user enters a letter instead of an integer into the switch case code. Patch soon to come.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -22,15 +22,24 @@ void insertNode(node * head, int x){
     node * newNode = new node;
     newNode->x = x;
     newNode->next = nullptr;
+    if(head == nullptr){
+        head = newNode;
+    }
+    else{
     node * current = head;
     while (current->next != nullptr){
         current = current->next;
     }
     current->next = newNode;
+    }
 }
 
-void deleteNode(node * head){
+void deleteNode(node * &head){
     if (head == nullptr) return;
+    else if (head->next == nullptr){
+        delete(head);
+        head = nullptr;
+    }
     else{
         node * current = head;
         while(current->next->next != nullptr){
@@ -42,17 +51,59 @@ void deleteNode(node * head){
 }
 
 void printLinkedList(node * head){
+    int nodeCounter = 1;
     while(head != nullptr){
-        cout << "This node = " << head->x << endl;
+        cout << "Node " << nodeCounter << " = " << head->x << endl;
         head = head->next;
+        nodeCounter++;
     }
 }
 
+void exitListApp(node * head){
+    node * nextNode;
+    while(head != nullptr){
+        nextNode = head->next;
+        delete(head);
+        head = nextNode;
+    }
+}
+
+
+// I'm thinking for the app now that I've got the bulk of the linked list logic
+// finished I should work on both refining the functions above as well as focusing
+// on making the CLI a little user friendly and intuitive. I will be adding a print
+// list option but I could always just print it every loop so the user automatically
+// sees their current list
 int main(){
-    node * head = createHead(21);
-    insertNode(head, 24);
-    printLinkedList(head);
-    deleteNode(head); // simply popping the last node from the tail
-    delete(head); // actually deleting the head;
+    int menu = 0;
+    int x = 0;
+    node * head = nullptr;
+    cout << endl << "Welcome to the Linked List App!" << endl << endl;
+    while(menu != 3){
+        cout << "----- Menu -----\n1.) Add Node\n2.) Delete Node\n3.) Exit\n";
+        cin >> menu;
+        switch(menu) {
+            case 1:
+                cout << "Assign a value to this Node: ";
+                cin >> x;
+                cout << endl;
+                insertNode(head, x);
+                cout << "Node added successfully" << endl << endl;
+                break;
+            case 2:
+                deleteNode(head);
+                cout << "Node deleted successfully" << endl << endl;
+                break;
+            case 3:
+                exitListApp(head);
+                cout << endl << "--- Goodbye! ---" << endl << endl;
+                break;
+            default:
+                cout << "Invalid Number. Please select a number from the Menu..." << endl;
+                break;
+        }
+        printLinkedList(head);
+    }
+    delete(head); // manual garbage collection
     return 0;
 }
